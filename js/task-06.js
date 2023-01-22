af5ca551fbe76c4c4386fbc1e986f4d2cab00dac
dusixx <113414935+dusixx@users.noreply.github.com>
2023-01-22 07:24:56 +0200

    added a second variant

--- js/task-06.js
+++ js/task-06.js
@@ -1,3 +1,6 @@
+//
+// Вариант 1
+//
 const onValidationInputBlur = ({ currentTarget: inputRef }) => {
   // приходит числовой строкой
   const validLen = Number(inputRef.dataset.length);
@@ -19,6 +22,20 @@ const onValidationInputBlur = ({ currentTarget: inputRef }) => {
       classList.add("invalid");
       classList.remove("valid");
   }
+};
+
+//
+// Вариант 2
+//
+const onValidationInputBlur2 = ({ currentTarget: inputRef }) => {
+  const COLOR_VALID = "#4caf50";
+  const COLOR_INVALID = "#f44336";
+
+  const validLen = +inputRef.dataset.length;
+  const currentLen = inputRef.value.length;
+
+  inputRef.style.borderColor =
+    currentLen === 0 ? null : currentLen === validLen ? COLOR_VALID : COLOR_INVALID;
 };
 
 document.querySelector("#validation-input").addEventListener("blur", onValidationInputBlur);
