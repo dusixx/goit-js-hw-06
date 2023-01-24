35a19a1b68454f57d1e3f3922e57ea7062aece20
dusixx <113414935+dusixx@users.noreply.github.com>
2023-01-24 09:30:30 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,32 +2,12 @@ const onValidationError = fieldName => {
   alert(`Error: No empty fields allowed (${fieldName})`);
 };
 
-//
-// Вариант 1 (elements)
-//
-const getFormData = (form, onError) => {
-  const formData = {};
-
-  for (const { tagName, value, name } of form.elements) {
-    if (tagName.toLowerCase() !== "input") continue;
-
-    if (value.length === 0) return onError(name);
-
-    formData[name] = value;
-  }
-
-  return formData;
-};
-
-//
-// Вариант 2 (FormData)
-//
-const getFormData2 = (form, onError) => {
+const getFormData = (form, onValidationError) => {
   const result = {};
   const formData = new FormData(form);
 
   for (const [name, value] of formData) {
-    if (value.length === 0) return onError(name);
+    if (value.length === 0) return onValidationError(name); // undefined
 
     result[name] = value;
   }
@@ -43,7 +23,7 @@ const onLoginFormSubmit = event => {
   event.preventDefault();
 
   const { currentTarget: form } = event;
-  const formData = getFormData2(form, onValidationError);
+  const formData = getFormData(form, onValidationError);
 
   if (formData) {
     console.log("formData", formData);
