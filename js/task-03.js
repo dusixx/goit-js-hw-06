4aa412599e6103799c1fac2fe485108fb2a6f9a7
dusixx <113414935+dusixx@users.noreply.github.com>
2023-02-17 16:52:18 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -2,7 +2,8 @@ import { galleryItems } from "./utils.js";
 
 const createGallery = (images, parentClass) => {
   const galleryRef = document.querySelector(`.${parentClass}`);
-  if (!galleryRef) return;
+
+  if (!galleryRef) return null;
 
   galleryRef.insertAdjacentHTML(
     "beforeend",
