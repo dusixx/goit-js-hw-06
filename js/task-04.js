ed637e4f71cdf7ecfac22bfde0178d9d32e7c624
dusixx <113414935+dusixx@users.noreply.github.com>
2023-02-17 16:50:52 +0200

    small fixes

--- js/task-04.js
+++ js/task-04.js
@@ -6,18 +6,3 @@ let counterValue = 0;
 
 incBtnRef.addEventListener("click", () => (counterValueRef.textContent = counterValue += 1));
 decBtnRef.addEventListener("click", () => (counterValueRef.textContent = counterValue -= 1));
-
-//
-// Вариант 2
-//
-// const onIncrementButtonClick = ({
-//   currentTarget: {
-//     dataset: { action },
-//   },
-// }) => {
-//   counterValue += action === "increment" ? 1 : -1;
-//   refs.counterValue.textContent = counterValue;
-// };
-
-// refs.incBtn.addEventListener("click", onIncrementButtonClick);
-// refs.decBtn.addEventListener("click", onIncrementButtonClick);
--- js/task-06.js
+++ js/task-06.js
@@ -11,19 +11,3 @@ document
     const cls = currentLen === validLen ? "invalid valid" : "valid invalid";
     utils.replaceClass(inputRef, ...cls.split(" "));
   });
-
-//
-// Вариант 2
-//
-// const onInputBlur = ({ currentTarget: inputRef }) => {
-//   const COLOR_VALID = "#4caf50";
-//   const COLOR_INVALID = "#f44336";
-
-//   const validLen = +inputRef.dataset.length;
-//   const currentLen = inputRef.value.length;
-
-//   inputRef.style.borderColor =
-//     currentLen === 0 ? null : currentLen === validLen ? COLOR_VALID : COLOR_INVALID;
-// };
-
-// document.querySelector("#validation-input")?.addEventListener("blur", onInputBlur);
